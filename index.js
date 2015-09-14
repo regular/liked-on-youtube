043ae8f2a5a7b1dde424229c88758c70b7eb41bc
Jan Bölsche <regular.gonzales@googlemail.com>
2015-09-14 15:02:32 +0200

    Use new pathway-based API of pull-paginated-api-request

--- index.js
+++ index.js
@@ -1,77 +1,48 @@
-var hyperquest = require('hyperquest');
+var paginated = require('pull-paginated-api-request');
+var pull = require('pull-stream');
 var querystring = require('querystring');
 var extend = require('xtend');
-var json = require('JSONStream');
-var map = require('flat-map');
-var through = require('through');
-var debug = require('debug')('liked-on-youtube');
+var debug = require('debug')('pull-liked-on-youtube');
+var hyperquest = require('hyperquest');
 
-function ytr(prefix, key) {
-    // return a function that retuens a stream of objects
-    // (see JSONStream for jsonPath argument)
-    // if nextPageToken is found in the API response,
-    // the next page is requested automatically.
-    return function request(url, query, jsonPath) {
-        var stream = through();
-        var out1 = json.parse(jsonPath);
-        out1.pipe(stream);
+var base_url = "https://www.googleapis.com/youtube/v3/";
 
-        function r(pageToken) {
-            var more = false;
-            var out2 = json.parse(['nextPageToken']);
-            out2.pipe(through(function(nextPageToken) {
-                debug('we have a next page:', nextPageToken);
-                more = true;
-                r(nextPageToken);
-            }));
-            debug('requesting page', pageToken);
-            hyperquest(
-                prefix + url + '?' + querystring.stringify(
-                    extend(query, {key: key}, pageToken ? {pageToken: pageToken} : {})
-                )
-            ).pipe(through(function(data) {
-                out1.write(data);
-                out2.write(data);
-            }, function() {
-                out2.end();
-                if (!more) {
-                    debug('no more pages');
-                    out1.end();
-                }
-                this.push(null);
-            }));
-        }
-        r();
-        return stream;
+module.exports = function(api_key, opts) {
+    opts = opts || {};
+    var makeRequest = opts.request || hyperquest;
+    var request = paginated(function(query, pageToken) {
+        var endpoint = query.endpoint; delete query.endpoint;
+        return makeRequest(base_url + endpoint + '?' + querystring.stringify(
+            extend({key: api_key}, query, pageToken ? {pageToken: pageToken} : {})
+        ));
+    });
+
+    return function(id) {
+        return pull(
+            request({
+                endpoint: 'channels',
+                part: 'contentDetails',
+                id: id
+            },
+                [['items', 0, 'contentDetails', 'relatedPlaylists', 'likes']]
+            ),
+            pull.asyncMap(function(id, cb) {
+                // id = playlistID of the 'likes' pseudo-playlist
+                debug('likes playlist id: %s', id);
+                cb(null, 
+                    request({
+                        endpoint: 'playlistItems',
+                        part: 'snippet',
+                        playlistId: id,
+                        maxResults: 50
+                    },
+                    [
+                        ['items', true], 
+                        ['nextPageToken']
+                    ])
+                );
+            }),
+            pull.flatten()
+        );
     };
-}
-
-var request = ytr(
-    "https://www.googleapis.com/youtube/v3/",
-    'AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU'
-);
-        
-module.exports = function(id) {
-    return request('channels', {
-        part: 'contentDetails',
-        id: id
-    }, [
-        'items',
-        true,
-        'contentDetails',
-        'relatedPlaylists',
-        'likes'
-    ]).pipe(map(function(id, cb) {
-        // id = playlistID of the 'likes' pseudo-playlist
-        debug('likes playlist id:', id);
-        cb(null, request('playlistItems', {
-            part: 'snippet',
-            playlistId: id,
-            maxResults: 50
-        }, [
-            'items', 
-            true
-        ]));
-    }));
 };
-
--- test.js
+++ test.js
@@ -1,16 +1,83 @@
-var likes = require('.');
-var through = require('through');
+var Likes  = require('./');
 
 var test = require('tape');
+var pull = require('pull-stream');
+var bl = require('bl');
 var debug = require('debug')('test');
 
-test('should return list of liked videos', function(t) {
-  var i = 0;
-  likes('UCsoqfkGvfd2Wy4FQFelTo5g').pipe(through(function(data) {
-    ++i;
-    debug(data.snippet.title);
-  }, function() {
-    t.assert(i>200, 'should be a fair number of liked videos');
-    t.end();
-  }));
+function from(str) {
+    var b = new bl();
+    b.append(str);
+    return b;
+}
+
+function requestMock(responses, cb) {
+    var i = 0;
+    return function (url) {
+        debug('requested url: %s', url);
+        cb(i, url);
+        if (i>responses.length-1) throw new Error('Out of responses!');
+        debug('response: %s', responses[i]);
+        return from(responses[i++]);
+    };
+}
+
+function likes(t, username, responses, urls, items) {
+    return Likes('secret', {
+        request: requestMock(responses, function(i, url) {
+            if (i > urls.length - 1) return t.fail();
+            t.equal(url,urls[i]); 
+        })
+    })(username);
+}
+
+test('should handle parse error gracefully', function(t) {
+    pull(
+        likes(t, 
+            'user', 
+            ['glibberish'], 
+            ["https://www.googleapis.com/youtube/v3/channels?key=secret&part=contentDetails&id=user"]
+        ),
+        pull.onEnd(function(err) {
+            debug(err.message);
+            t.ok(err);
+            t.end();
+        })
+    );
 });
+
+test('should handle missing properties gracefully (1)', function(t) {
+    pull(
+        likes(t, 
+            'user', 
+            ['{"items":[{}]}'], 
+            ["https://www.googleapis.com/youtube/v3/channels?key=secret&part=contentDetails&id=user"]
+        ),
+        pull.collect(function(err, values) {
+            t.equal(err, null);
+            t.deepEqual(values, []);
+            t.end();
+        })
+    );
+});
+
+test('should handle missing properties gracefully (2)', function(t) {
+    pull(
+        likes(t, 
+            'user', 
+            [
+             '{"items":[{"contentDetails": {"relatedPlaylists": {"likes": "likes-id"}}}]}',
+             '{}'
+            ],
+            [
+             "https://www.googleapis.com/youtube/v3/channels?key=secret&part=contentDetails&id=user",
+             "https://www.googleapis.com/youtube/v3/playlistItems?key=secret&part=snippet&playlistId=likes-id&maxResults=50"
+            ]
+        ),
+        pull.onEnd(function(err) {
+            t.notOk(err);
+            t.end();
+        })
+    );
+});
+
